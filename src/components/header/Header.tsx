bb08f78d57ec215952ac5ed4f1ca0a55c939a8c7
Mustafa <mostafakhaled0787314@gmail.com>
2024-02-23 13:21:59 +0200

    change logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,12 @@
 import ToggleTheme from "./ToggleTheme";
+import logoImage from "/public/logo.png";
 
 export default function Header() {
   return (
     <div className="h-[50px] flex items-center bg-secondary">
       <div className="container flex items-center justify-between mx-auto px-[10px]">
         <div>
-          <h2 className="font-bold text-xl">Kanban</h2>
+          <img src={logoImage} alt="logo" />
         </div>
         <ToggleTheme />
       </div>
